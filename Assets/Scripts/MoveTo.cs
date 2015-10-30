17c5779ec1ee209201351d6c874bbb2337edc213
Carolina <carolinaisaza1>
2015-10-29 20:34:03 -0500

    velocidad igual en jugadores

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class MoveTo : MonoBehaviour
 {
 	NavMeshAgent agent;
-	//Save save = new Save ();
+    //Save save = new Save ();
 	//float distance = 0;
 	//NavMeshPath path;
 
 	void Start ()
 	{
 		agent = GetComponent<NavMeshAgent> ();
-		//path = new NavMeshPath ();
-	}
+        //path = new NavMeshPath ();
+       /// mat.connection();
+
+    }
 
 	void Update ()
 	{	
@@ -22,6 +24,8 @@ public class MoveTo : MonoBehaviour
 
 			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100)) {
 				agent.destination = hit.point;
+              
+
 				//agent.CalculatePath (hit.point, path);
 				//distance = path.corners[0].magnitude;
 				/*for (int i = 0; i < path.corners.Length-1; i++) {
